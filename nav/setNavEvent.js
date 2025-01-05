6afc822715ae69e9a8971cb84725e2ef19eeaa75
임재원 <nemo0824@naver.com>
2025-01-05 20:09:09 +0900

    fix 1.navbar 동적으로 관리할수있도록 app 요소안에서 동적으로 렌더링 (app 외부에서 고정 ==> app header 에서 동적으로 처리) 2.event요소 선택자 기준 관리(tagname => class) 3.rotue, mock, 구조 등 네이밍에 맞게 변경

--- nav/setNavEvent.js
+++ nav/setNavEvent.js
@@ -2,7 +2,7 @@ export function setNavEvent() {
   const navbar = document.querySelector('#navbar');
 
   navbar.addEventListener('click', (e) => {
-    if (e.target.tagName === 'A' && e.target.hasAttribute('data-link')) {
+    if (e.target.classList.contains('nav-link')) {
       e.preventDefault();
       const url = e.target.getAttribute('href');
       navigate(url);
