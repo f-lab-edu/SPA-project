f483e3c1e86e1a4360c444f17b14f1bf82562007
nemo0824 <59534382+nemo0824@users.noreply.github.com>
2025-01-05 17:02:20 +0900

    refactor (#4)
    * refactor
    router router/render 분리 -> 추상화
    js 폴더 정리
    이벤트 navbar  교체  -> 오류방지
    
    * fix
    1.navbar 동적으로 관리할수있도록 app 요소안에서 동적으로 렌더링
    2.event요소 선택자 기준 관리
    3.rotue, mock, 구조 등 네이밍에 맞게 변경

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-import { setupRouter } from './router.js';
-import { setNavEvent } from './navbar.js';
+import { setupRouter, renderNav } from './router/router.js';
+import { setNavEvent } from './nav/setNavEvent.js';
 
 // 초기화함수
 async function initApp() {
   try {
+    const header = document.getElementById('header');
+    header.appendChild(renderNav());
+
     setNavEvent();
     setupRouter();
   } catch (error) {
---
+++ router/router.js
@@ -0,0 +1,85 @@
+import {
+  fetchCategoryData,
+  fetchDetailData,
+  fetchAllData,
+} from '../service/dataservice.js';
+
+const routes = {
+  '/': 'main',
+  '/detail': 'detail',
+  '/develop': 'category',
+  '/design': 'category',
+};
+// main
+async function renderMainPage() {
+  const data = await fetchAllData();
+  return `
+   <ul>
+      ${data.map((item) => `<li><a href="/detail?id=${item.id}" class="router-link">${item.title}</a></li>`).join('')}
+   </ul>
+      `;
+}
+// category
+async function renderCategoryPage(category) {
+  const data = await fetchCategoryData(category);
+  return `
+      <ul>
+        ${data.map((item) => `<li><a href="/detail?id=${item.id}" class="router-link">${item.title}</a></li>`).join('')}
+      </ul>
+    `;
+}
+// detail
+async function renderDetailPage() {
+  const params = new URLSearchParams(window.location.search);
+  const id = params.get('id');
+  const item = await fetchDetailData(id);
+  return `<h3>${item.title}</h3><p>${item.content}</p>`;
+}
+
+// nav
+export function renderNav() {
+  const nav = document.createElement('nav');
+  nav.id = 'navbar';
+  nav.innerHTML = `
+  <a href="/" class="nav-link">전체</a>
+  <a href="/develop" class="nav-link">개발</a>
+  <a href="/design" class="nav-link">디자인</a>
+  `;
+
+  return nav;
+}
+
+// 렌더링
+async function renderPage(route) {
+  const content = document.getElementById('content');
+
+  switch (routes[route]) {
+    case 'main':
+      content.innerHTML = await renderMainPage();
+      break;
+    case 'detail':
+      content.innerHTML = await renderDetailPage();
+      break;
+    case 'category':
+      const category = route.replace('/', '');
+      content.innerHTML = await renderCategoryPage(category);
+      break;
+  }
+}
+
+export function setupRouter() {
+  renderPage(location.pathname);
+
+  window.addEventListener('popstate', () => {
+    renderPage(location.pathname);
+  });
+
+  document.addEventListener('click', (e) => {
+    if (e.target.classList.contains('router-link')) {
+      e.preventDefault();
+      const url = e.target.getAttribute('href');
+      history.pushState(null, null, url);
+      renderPage(new URL(url, location.origin).pathname);
+    }
+  });
+}
